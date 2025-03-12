9e6b9ab28bd6b775a8dfe1720029c1f4ccaf9661
Ayat Tulloh Rahulloh Khomeini <5026211156@mhs.its.ac.id>
2025-03-12 19:55:18 +0700

    feat: update sentiment analysis pipeline. compare tokens only between tokens_stopword datas. imbalanced data (BOTH)

--- code/pipeline/3_labeling.py
+++ code/pipeline/3_labeling.py
@@ -1,12 +1,30 @@
 import pandas as pd
 from transformers import pipeline
+# Correct model identifier
+model_name = "w11wo/indonesian-roberta-base-sentiment-classifier"
 
-#load pretrained model
-classifier = pipeline('sentiment-analysis', model="W11wo/indonesian-roberta-large-sentiment-classifier")
+# Initialize the sentiment analysis pipeline
+classifier = pipeline("sentiment-analysis", model=model_name)
 
-#load data
+#-----------------------------------------------------------------------------
+#load data tokens
+#-----------------------------------------------------------------------------
 df = pd.read_csv('../../data/output/tokens.csv')
 
 df['Sentiment'] = df['Text'].apply(lambda x: classifier(x)[0]['label'])
 
 print("Sentiment analysis completed successfully!")
+
+#------------------------------------------------------------
+# Load data tokens and stopwords removed
+#------------------------------------------------------------
+df_stopwords = pd.read_csv('../../data/output/tokens_no_stopword.csv')
+df_stopwords['Sentiment'] = df_stopwords['Text'].apply(lambda x: classifier(x)[0]['label'])
+print("Sentiment analysis completed successfully!")
+
+#------------------------------------------------------------
+# visualize the data
+#------------------------------------------------------------
+
+print(df['Sentiment'].value_counts())
+print(df_stopwords['Sentiment'].value_counts())
