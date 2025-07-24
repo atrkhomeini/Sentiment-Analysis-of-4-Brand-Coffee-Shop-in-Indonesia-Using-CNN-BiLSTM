d45b8cc9052b7cecacad2dd1c6d91617977c9658
Ayat Tulloh Rahulloh Khomeini <5026211156@mhs.its.ac.id>
2025-07-24 16:56:47 +0700

    fix: update dataset path and modify brand filtering logic for Fore tweets

--- src/pipeline/3_elimination.py
+++ src/pipeline/3_elimination.py
@@ -1,14 +1,14 @@
 import pandas as pd 
 
 # Load dataset
-df = pd.read_csv('../data/output/normalized.csv')
+df = pd.read_csv('../../data/output/kopken_normalized.csv')
 
 # ---------------------------------------------------------------------------------------
 # Filter tweet yang menyebut Fore dan bukan hanya daftar brand
 # ---------------------------------------------------------------------------------------
 
 # Fokus hanya ke brand Fore
-target_brand = 'fore'
+target_brands = ['kopi kenangan', 'kopken']
 brands = ['fore', 'kopi kenangan', 'point coffee', 'tomoro', 'kopken']
 
 def detect_brands(text):
@@ -22,18 +22,18 @@ def is_brand_only(text):
 filtered_rows = []
 for _, row in df.iterrows():
     text = row['Text Normalization']
-    if target_brand in text.lower() and not is_brand_only(text):
+    if target_brands in text.lower() and not is_brand_only(text):
         filtered_rows.append({
             'Date': row['Date'],
             'Text': row['Text'],  # Tambahkan kolom Text asli
             'Text Normalization': text,
-            'Brand': target_brand,
+            'Brand': target_brands,
         })
 
 # Buat DataFrame hasil
 df_expanded = pd.DataFrame(filtered_rows)
 
 # Simpan hasil ekspansi
-path_save = '../data/output'
-df_expanded.to_excel(f'{path_save}/elimination.xlsx', index=False)
-print('Elimination data saved to ../data/output/elimination.csv')
+path_save = '../../data/output'
+df_expanded.to_excel(f'{path_save}/kopken_elimination.xlsx', index=False)
+print('Elimination data saved to ../data/output/kopken_elimination.csv')
