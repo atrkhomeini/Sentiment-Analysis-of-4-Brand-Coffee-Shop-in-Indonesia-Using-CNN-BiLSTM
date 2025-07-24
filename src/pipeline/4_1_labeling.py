b2849bc8ec02bc2f41c2b1a71f78a3cbc07f43a0
Ayat Tulloh Rahulloh Khomeini <5026211156@mhs.its.ac.id>
2025-07-24 16:56:56 +0700

    fix: update data loading path and save results to Excel for IndoBERT labeling

--- src/pipeline/4_1_labeling.py
+++ src/pipeline/4_1_labeling.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 import matplotlib.pyplot as plt
 
 # Load the data
-df = pd.read_csv('../data/output/elimination.csv')
+df = pd.read_csv('../../data/output/normalized.csv')
 
 # -----------------------------------------
 # Labeling Data with IndoBERT only
@@ -23,12 +23,34 @@ df_bert['label_bert'] = df_bert['Text'].apply(lambda x: bert_model(x)[0]['label'
 # -----------------------------------------
 
 # Filter kolom yang relevan
-df_result = df_bert[['Date', 'Text', 'Brand', 'label_bert']]
+df_result = df_bert[['Date', 'Text','Text Normalization', 'label_bert']]
 
 # Rename kolom untuk konsistensi
 df_result.rename(columns={'label_bert': 'Sentimen'}, inplace=True)
 
 # Simpan ke file CSV baru
-df_result.to_csv('../data/output/labelled.csv', index=False)
+df_result.to_excel('../../data/output/fore_labelled.xlsx', index=False)
 
-print('Labelling data saved to ../data/output/labelled.csv')
+print('Labelling data saved to ../../data/output/fore_labelled.xlsx')
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+df = pd.read_excel('../../data/use/merge/labelled_merge.xlsx', sheet_name='Sheet1')
+# Visualisasi Distribusi Sentimen
+plt.figure(figsize=(8, 6))
+ax = sns.countplot(data=df, x='Sentimen', palette='viridis', order=df['Sentimen'].value_counts().index)
+plt.title('Distribusi Sentimen Hasil Pelabelan IndoBERT')
+plt.xlabel('Sentimen')
+plt.ylabel('Jumlah Data')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+# Add data labels on bars
+for container in ax.containers:
+    ax.bar_label(container, label_type='edge')
+
+plt.tight_layout()
+plt.show()
+# Anda juga bisa mencetak jumlah pastinya:
+print("\nJumlah tweet per kategori sentimen setelah pelabelan IndoBERT:")
+print(df['Sentimen'].value_counts())
